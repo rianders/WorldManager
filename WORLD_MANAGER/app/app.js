7aa7aa760f0d7d5ef6c0f5bc8ea31101a4aa1459
Ryan Ostrager <rostrage@eden.rutgers.edu>
2012-10-12 15:23:42 -0400

    Hello World
    Got express to use handlebar.js and twitter bootstrap.

--- WORLD_MANAGER/app/app.js
+++ WORLD_MANAGER/app/app.js
@@ -9,55 +9,34 @@ var express = require('express')
   , cons = require('consolidate')
   , Handlebars = require('handlebars')
   , request = require('request')  
-  , app = express();
+  , app = express()
+  , fs = require('fs');
 
-//require('express-handlebars')(app, Handlebars);
+app.configure('development', function () {
+	app.use(express.logger());
+	app.use(express.errorHandler({
+		dumpExceptions: true,
+		showStack: true}))
+	});
 
-app.engine('html', require('express-handlebars'));
+var hbs = require('hbs');
 
-// set .html as the default extension 
-app.set('view engine', 'html');
+// set .hbs as the default extension 
+app.set('view engine', 'hbs');
+app.engine('hbs', cons.handlebars);
 app.set('views', __dirname + '/views');
+app.set('view options', {layout:false});
+app.use(express.static(__dirname+'/static'));
+
+
+//Handlebars partials & helpers
+
+Handlebars.registerPartial('grid', fs.readFileSync(__dirname + '/views/partials/grid.hbs', 'utf8'));
 
 
 app.get('/', function(req, res) {
-    res.sendfile("public/awdt.html");
+    res.render('test');
 });
-
-//Gets data from youtube and return to client
-app.get('/getyoutube', function(req, res) {
-    var url = 'https://gdata.youtube.com/feeds/api/videos?';
-    var query = req.param('q');
-    var params = 'q=' + query + 'alt=json';
-    url = url + params;
-
-    console.log("query: " + req.param('q'));
-
-    request(url, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body) // Print page to console
-            res.send(body);
-        }
-    });
-
-});
-
-app.get('/searchtomatoes', function(req, res) {
-    var key = "wvvcz974q8ytn7am97y6t4x5";
-    var query = req.param("q");
-    var url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey="
-+ key + "&q=" + query + "&page_limit=3&page=1";
-
-    request(url, function(error, response, body) {
-        console.log("URL: " + url);
-        if (!error && response.statusCode == 200) {
-            res.send(body);
-        }
-    });
-
-});
-//app.get('/getgiantbomb', function(req, res) {
-
 
 var port = 3000;
 app.listen(port);
