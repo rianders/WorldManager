fd39d0adf65a6956fd60ee0a157d469ee87ef571
Zachary Pinner <z.e.pinner@gmail.com>
2012-12-19 12:12:06 -0500

    Information added in Upload now shows  up on Home screen

--- app/app.js
+++ app/app.js
@@ -1,12 +1,3 @@
-/*
- To use this file:
- npm install express
- npm install request
- npm install handlebars
- npm install mongoose
- npm install fs
- npm install mongous
-*/
 var express = require('express')
   , util = require('util')
   , cons = require('consolidate')
@@ -73,14 +64,12 @@ fs.readdir(partialsDir, function(err, files) {
 		filename=files[filename];
 //check that the file is a handlebars file
      		var filetype = path.extname(filename);
-     		console.log("registering file: " + filename);
+			console.log("registering file: " + filename);
      if (filetype==".hbs") {
-		  var name = path.basename(filename, filetype);
-		  //var info = path.basename(filename, filetype);								//INFO COPYING NAME
+		  var name = path.basename(filename, filetype);	
 		  var template = fs.readFileSync(partialsDir + '/' + filename, 'utf8');
 		  console.log(name);
 		  Handlebars.registerPartial(name, template);
-		  //Handlebars.registerPartial(info, template);									//INFO COPYING NAME
 		}
 	}
 	});
@@ -159,32 +148,20 @@ app.get('/builds/:id', function(req,res) {
 
 app.post('/', function(req, res, next){
 	if(req.isAuthenticated())
-	{
-		console.log("Received new world!");
-		
-		//var extension = path.extname(req.files.build.name.info);																//INFO COPYING NAME
-		var extension = path.extname(req.files.build.name);
-		
+	{		
+		var extension = path.extname(req.files.build.name);																		
 		if(extension == ".unity3d") {
-			console.log(req.body);
-			console.log("End of body");
 			newWorld = req.body;
 			newWorld.id = path.basename(req.files.build.path);
-			
-			//newWorld.world = "/builds/"+newWorld.id+"/"+req.files.build.name.info;												//INFO COPYING NAME
-			newWorld.world = "/builds/"+newWorld.id+"/"+req.files.build.name;
-			
-			//newWorld.img = "/img/"+newWorld.id+"/"+req.files.image.name.info;													//INFO COPYING NAME
-			newWorld.img = "/img/"+newWorld.id+"/"+req.files.image.name;
-			
+			newWorld.world = "/builds/"+newWorld.id+"/"+req.files.build.name;												
+			newWorld.img = "/img/"+newWorld.id+"/"+req.files.image.name;																			
 			newWorld.href = "/builds/"+newWorld.id;
-			newWorld.user = req.user;
 			fs.mkdirSync(__dirname+"/static/img/"+newWorld.id);
 			fs.mkdirSync(__dirname+"/static/builds/"+newWorld.id);
+			
 			fs.readFile(req.files.build.path, function(err, data) {
 				
-				//fs.writeFile(__dirname+"/static/builds/"+newWorld.id+"/"+req.files.build.name.info, data, function (err) {		//INFO COPYING NAME
-				fs.writeFile(__dirname+"/static/builds/"+newWorld.id+"/"+req.files.build.name, data, function (err) {
+				fs.writeFile(__dirname+"/static/builds/"+newWorld.id+"/"+req.files.build.name, data, function (err) {		
 					
 					if(err) throw err;
 					res.redirect("/");
@@ -192,13 +169,12 @@ app.post('/', function(req, res, next){
 			});
 			fs.readFile(req.files.image.path, function(err, data) {
 				
-				//fs.writeFile(__dirname+"/static/img/"+newWorld.id+"/"+req.files.image.name.info, data, function (err) {			//INFO COPYING NAME
-				fs.writeFile(__dirname+"/static/img/"+newWorld.id+"/"+req.files.image.name, data, function (err) {	
+				fs.writeFile(__dirname+"/static/img/"+newWorld.id+"/"+req.files.image.name, data, function (err) {		
 					
 					if(err) throw err;
 				});
-			});
-			$(config.db+".worlds").save(newWorld);
+			});	
+			$(config.db+".worlds").save(newWorld)
 		}
 	}
 	else
@@ -216,13 +192,10 @@ app.get('/auth/google/return',
   });
   
 app.get('/upload', function(req, res, next){
-console.log("in upload get");
 if(req.isAuthenticated())
 {
 	var formData = {};
 	formData.upload=true;
-	formData.form=[{desc:"Name", type:"text", name:"name"}, {desc:"Info", type:"textarea", info:"info"}, {desc:"Build", type: "file", name:"build"}, {desc:"Preview", type: "file", name:"image"}];
-	console.log("in upload if");
 	res.render('root', formData);
 }
 else
@@ -230,6 +203,13 @@ else
 	res.redirect('/login');
 }
 });
+
+function defaultForm(){
+	return false;
+}
+function customForm(){
+	return true;
+}
 
 app.get('/contact', function(req, res, next){
 	res.redirect('http://rutgers-virtual-worlds.tenderapp.com/');
